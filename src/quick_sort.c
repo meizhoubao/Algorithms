ac445a8fadc67f5b7725a65692e2f6cf9c123831
Leopard Li <18032773381@163.com>
2017-02-08 10:10:24 +0800

    Update

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -1,9 +1,10 @@
 #include "quick_sort.h"
 
-static inline int partition(int a[], int p, int q)
+// My solution for PARTITION
+static inline int partition_1(int a[], int p, int q)
 {
 	int x = a[p];
-	int i = p, j = p, tmp;
+	int i = p, j = p;
 	for (j = j + 1; j <= q; j++) {
 		if (a[j] < x) {
 			a[i] = a[j];
@@ -18,8 +19,35 @@ void quick_sort_1(int a[], int p, int q)
 {
 	int k;
 	if (p < q) {
-		 k = partition(a, p, q);
+		 k = partition_1(a, p, q);
 		 quick_sort_1(a, p, k-1);
 		 quick_sort_1(a, k+1, q);
+	}
+}
+
+// MIT 6.046J course solution
+static inline int partition_2(int a[], int p, int q)
+{
+	int x = a[p];
+	int i = p, j, tmp;
+	for (j = i + 1; j <= q; j++) {
+		if (a[j] <= x) {
+			tmp = a[j];
+			a[j] = a[++i];
+			a[i] = tmp;
+		}
+	}
+	a[p] = a[i];
+	a[i] = x;
+	return i;
+}
+
+void quick_sort_2(int a[], int p, int q)
+{
+	int k;
+	if (p < q) {
+		k = partition_2(a, p, q);
+		quick_sort_2(a, p, k-1);
+		quick_sort_2(a, k+1, q);
 	}
 }
--- src/quick_sort.h
+++ src/quick_sort.h
@@ -2,5 +2,6 @@
 #define __ALGORITHM_QUICK_SORT_H
 
 void quick_sort_1(int a[], int p, int q);
+void quick_sort_2(int a[], int p, int q);
 
 #endif
